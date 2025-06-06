dropbox-code/repo-5

Improve some interfaces about ORC filter push down (#11)
* improve some interfaces about orc search argument

* remove std::cout

* fix invalid memory access which cause https://s3.amazonaws.com/clickhouse-test-reports/55330/7b328df6f65f56b5c2944bc1f283d4f376cc783c/stateless_tests__msan__[3_6].html

* fix code style

* fix code style

* improve code

* allow in single literals

* fix failed uts after allowing in literal list with size 1

* fix failed windows ut

c++/src/sargs/ExpressionTree.cc
@@ -19,6 +19,7 @@
 #include "ExpressionTree.hh"
 
 #include <cassert>
+#include <iostream>
 #include <sstream>
 
 namespace orc {
@@ -35,18 +36,18 @@ namespace orc {
   }
 
   ExpressionTree::ExpressionTree(size_t leaf)
-      : mOperator(Operator::LEAF), mChildren(), mLeaf(leaf), mConstant(TruthValue::YES_NO_NULL) {
+      : mOperator(Operator::LEAF), mLeaf(leaf), mConstant(TruthValue::YES_NO_NULL) {
     // PASS
   }
 
   ExpressionTree::ExpressionTree(TruthValue constant)
-      : mOperator(Operator::CONSTANT), mChildren(), mLeaf(UNUSED_LEAF), mConstant(constant) {
+      : mOperator(Operator::CONSTANT), mLeaf(UNUSED_LEAF), mConstant(constant) {
     // PASS
   }
 
   ExpressionTree::ExpressionTree(const ExpressionTree& other)
       : mOperator(other.mOperator), mLeaf(other.mLeaf), mConstant(other.mConstant) {
-    for (TreeNode child : other.mChildren) {
+    for (const TreeNode& child : other.mChildren) {
       mChildren.emplace_back(std::make_shared<ExpressionTree>(*child));
     }
   }
@@ -64,13 +65,12 @@ namespace orc {
         const_cast<const ExpressionTree*>(this)->getChildren());
   }
 
-  const TreeNode ExpressionTree::getChild(size_t i) const {
+  const TreeNode& ExpressionTree::getChild(size_t i) const {
     return mChildren.at(i);
   }
 
-  TreeNode ExpressionTree::getChild(size_t i) {
-    return std::const_pointer_cast<ExpressionTree>(
-        const_cast<const ExpressionTree*>(this)->getChild(i));
+  TreeNode& ExpressionTree::getChild(size_t i) {
+    return mChildren.at(i);
   }
 
   TruthValue ExpressionTree::getConstant() const {

ClickHouse/orc

c++/src/sargs/ExpressionTree.cc
@@ -19,6 +19,7 @@
 #include "ExpressionTree.hh"
 
 #include <cassert>
+#include <iostream>
 #include <sstream>
 
 namespace orc {
@@ -35,18 +36,18 @@ namespace orc {
   }
 
   ExpressionTree::ExpressionTree(size_t leaf)
-      : operator_(Operator::LEAF), children_(), leaf_(leaf), constant_(TruthValue::YES_NO_NULL) {
+      : operator_(Operator::LEAF), leaf_(leaf), constant_(TruthValue::YES_NO_NULL) {
     // PASS
   }
 
   ExpressionTree::ExpressionTree(TruthValue constant)
-      : operator_(Operator::CONSTANT), children_(), leaf_(UNUSED_LEAF), constant_(constant) {
+      : operator_(Operator::CONSTANT), leaf_(UNUSED_LEAF), constant_(constant) {
     // PASS
   }
 
   ExpressionTree::ExpressionTree(const ExpressionTree& other)
       : operator_(other.operator_), leaf_(other.leaf_), constant_(other.constant_) {
-    for (TreeNode child : other.children_) {
+    for (const TreeNode& child : other.children_) {
       children_.emplace_back(std::make_shared<ExpressionTree>(*child));
     }
   }
@@ -64,13 +65,12 @@ namespace orc {
         const_cast<const ExpressionTree*>(this)->getChildren());
   }
 
-  const TreeNode ExpressionTree::getChild(size_t i) const {
+  const TreeNode& ExpressionTree::getChild(size_t i) const {
     return children_.at(i);
   }
 
-  TreeNode ExpressionTree::getChild(size_t i) {
-    return std::const_pointer_cast<ExpressionTree>(
-        const_cast<const ExpressionTree*>(this)->getChild(i));
+  TreeNode& ExpressionTree::getChild(size_t i) {
+    return children_.at(i);
   }
 
   TruthValue ExpressionTree::getConstant() const {